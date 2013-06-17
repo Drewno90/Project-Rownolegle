f641c3b7651961ff86bdd7d460a00420785833f3
tomasz <mmarecki1991@gmail.com>
2013-06-17 19:17:14 +0200

    CA Generacja random dla całej tablicy

--- Gra_w_zycie/CA.java
+++ Gra_w_zycie/CA.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 
 public class CA {
 
@@ -12,6 +14,16 @@ public class CA {
 			for (int j = 0; j < size; j++)
 				tab[i][j] = 0;
 
+	}
+	
+	/**
+	 * Generuje randomowe stany dla calej przestrzeni
+	 */
+	public void genRandomStates() {
+		Random rand = new Random();
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				tab[i][j] = rand.nextInt(2);
 	}
 
 
