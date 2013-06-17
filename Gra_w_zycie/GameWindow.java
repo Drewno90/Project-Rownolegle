a3fa69753c76995d4da5bd8b7c1e29032fab7708
Dawid <dawidwa@gmail.com>
2013-06-17 19:40:18 +0200

    Okno Gry

---
+++ Gra_w_zycie/GameWindow.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+
+
+public class GameWindow extends JPanel {
+
+	public GameWindow() {
+
+		setBorder(BorderFactory.createLineBorder(Color.YELLOW));
+
+		
+	}
+}
